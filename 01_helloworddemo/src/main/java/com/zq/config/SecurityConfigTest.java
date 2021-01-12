057f6b708dc9c2de90cf4a1ce12d1649b35680ad
zhangqi <zqloner@163.com>
2021-01-12 20:25:21 +0800

    ---  自定义403页面

--- 01_helloworddemo/src/main/java/com/zq/config/SecurityConfigTest.java
+++ 01_helloworddemo/src/main/java/com/zq/config/SecurityConfigTest.java
@@ -34,6 +34,8 @@ public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //配置没有权限访问的页面
+        http.exceptionHandling().accessDeniedPage("/unAuth.html");
         http.formLogin()   //自定义自己编写的登陆页面
                 .loginPage("/login.html")  //登陆页面设置
                 .loginProcessingUrl("/user/login") //登陆访问的路径
@@ -49,7 +51,7 @@ public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
 //                .hasRole()        针对某个角色
 //                .antMatchers("/test/index").hasRole("sale")
 //                .hasAnyRole()        针对多个个角色
-                .antMatchers("/test/index").hasAnyRole("sale")
+                .antMatchers("/test/index").hasAnyRole("unAuth")
                 .anyRequest().authenticated()
                 .and().csrf().disable();    //关闭csrf防护
     }
--- 01_helloworddemo/src/main/java/com/zq/service/MyUserDetailsService.java
+++ 01_helloworddemo/src/main/java/com/zq/service/MyUserDetailsService.java
@@ -1,19 +1,19 @@
 package com.zq.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.zq.entity.Users;
-import com.zq.mapper.UsersMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
+        import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+        import com.zq.entity.Users;
+        import com.zq.mapper.UsersMapper;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.security.core.GrantedAuthority;
+        import org.springframework.security.core.authority.AuthorityUtils;
+        import org.springframework.security.core.userdetails.User;
+        import org.springframework.security.core.userdetails.UserDetails;
+        import org.springframework.security.core.userdetails.UserDetailsService;
+        import org.springframework.security.core.userdetails.UsernameNotFoundException;
+        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+        import org.springframework.stereotype.Service;
 
-import java.util.List;
+        import java.util.List;
 
 /**
  * @Description:
