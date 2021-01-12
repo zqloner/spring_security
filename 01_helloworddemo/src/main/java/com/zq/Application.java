8975868a33da524c9573445b9e2ffd2e389ceb9f
zhangqi <zqloner@163.com>
2021-01-12 21:21:05 +0800

    ---  5中注解配置权限

--- 01_helloworddemo/src/main/java/com/zq/Application.java
+++ 01_helloworddemo/src/main/java/com/zq/Application.java
@@ -3,9 +3,11 @@ package com.zq;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @MapperScan("com.zq.mapper")
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled=true,prePostEnabled = true)
 public class Application {
 
     public static void main(String[] args) {
--- 01_helloworddemo/src/main/java/com/zq/controller/TestController.java
+++ 01_helloworddemo/src/main/java/com/zq/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.zq.controller;
 
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,15 @@ public class TestController {
     @GetMapping("index")
     public String index() {
         return "hello index";
+    }
+
+    @GetMapping("update")
+//    @Secured({"ROLE_sale","ROLE_manager"})
+//    @PreAuthorize("hasAnyAuthority('adminsss')")
+    @PostAuthorize("hasAnyAuthority('adminsss')")
+    public String update() {
+        System.out.println("update........");
+        return "hello update";
     }
 
 }
