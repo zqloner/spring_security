fc84570e8dbe21a7c155e651d25314aa46585069
zhangqi <zqloner@163.com>
2021-01-14 02:40:11 +0800

    ---  整合网关

---
+++ acl_parent/infrastructure/api_gateway/src/main/java/com/zq/gateway/GateWayApplication.java
@@ -0,0 +1,19 @@
+package com.zq.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/1/14--->2:18
+ * @Company: MGL
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
---
+++ acl_parent/infrastructure/api_gateway/src/main/java/com/zq/gateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.zq.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @Description:
+ * @Author: zhangqi
+ * @CreateTime: 2021/1/14--->2:20
+ * @Company: MGL
+ */
+/*
+解决跨域问题
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**",config);
+        return new CorsWebFilter(source);
+    }
+}
