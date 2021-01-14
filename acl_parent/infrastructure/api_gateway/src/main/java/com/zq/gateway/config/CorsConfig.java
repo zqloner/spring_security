92c690253b85c19ac8aa0137d8b1502465e4d122
zhangqi <zqloner@163.com>
2021-01-14 14:06:43 +0800

    ---  就觉扫描不到mapper.xml文件

--- acl_parent/infrastructure/api_gateway/src/main/java/com/zq/gateway/config/CorsConfig.java
+++ acl_parent/infrastructure/api_gateway/src/main/java/com/zq/gateway/config/CorsConfig.java
@@ -18,14 +18,18 @@ import org.springframework.web.util.pattern.PathPatternParser;
  */
 @Configuration
 public class CorsConfig {
+
+    //解决跨域
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedMethod("*");
         config.addAllowedOrigin("*");
-        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
         source.registerCorsConfiguration("/**",config);
+
         return new CorsWebFilter(source);
     }
 }
